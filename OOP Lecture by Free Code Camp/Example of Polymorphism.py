58cac975fab57d306a6ba773de793cfaade4e9c0
Harsh Raj <harshraj2717@gmail.com>
2022-01-19 16:42:03 +0530

    some message

---
+++ OOP Lecture by Free Code Camp/Example of Polymorphism.py
@@ -0,0 +1,10 @@
+s = "something"
+print(len(s))
+
+l = ["some", "thing"]
+print(len(l))
+
+'''
+That's Polymorphism ... a single function works in a different way for different functions.
+len() gives the number of characters if a str is passed but number of elements if a list/array is passed. 
+'''
